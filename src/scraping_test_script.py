4c59fc5f4ced5ed185e349e68e00581e46837765
renaudjester <renaud.jester@outlook.fr>
2023-11-08 11:48:40 +0100

    feat: create stories from code

--- src/scraping_test_script.py
+++ src/scraping_test_script.py
@@ -2,14 +2,15 @@ from container import Container
 
 
 def main():
-    chapters = Container.scrapper.get_all_chapters()
-    transcriptions = Container.scrapper.get_chapter_data(chapters.loc[0, "_id"])
+    # chapters = Container.scrapper.get_all_chapters()
+    # transcriptions = Container.scrapper.get_chapter_data(chapters.loc[0, "_id"])
 
-    transcriptName, transcriptData = Container.scrapper.get_transcription(
-        transcriptions[0]["_id"], chapters.loc[0, "_id"]
-    )
-    print(transcriptName)
-    print(transcriptData)
+    # transcriptName, transcriptData = Container.scrapper.get_transcription(
+    #     transcriptions[0]["_id"], chapters.loc[0, "_id"]
+    # )
+    # print(transcriptName)
+    # print(transcriptData)
+    Container.scrapper.save_to_story("6529456e77d0b130fde581ac", "rwrite this shit")
 
 
 if __name__ == "__main__":
